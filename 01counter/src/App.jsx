fbc2a0a235b413eea2bea6e5852488095cd288b7
ncodeinx9000 <66codewarrior99@gmail.com>
2026-03-23 13:19:07 +0530

    need of hooks and understand useState hook

---
+++ 01counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(15);
+
+  // let counter = 15;
+
+  const addValue = () => {
+    // counter = counter + 1;
+    if (counter >= 20) return;
+    setCounter(counter + 1);
+  };
+
+  const removeValue = () => {
+    if (counter <= 0) return;
+    setCounter(counter - 1);
+  };
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value : {counter}</button>
+      <br />
+      <button onClick={removeValue}>remove value : {counter}</button>
+    </>
+  );
+}
+
+export default App;
